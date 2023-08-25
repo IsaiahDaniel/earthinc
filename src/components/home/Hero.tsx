b1bde1af917f79671de646c25a1fb9f124ab19fd
Isaiah Daniel <isaiahdaniel203@gmail.com>
2023-08-25 12:04:26 +0100

    added focus effect

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,16 +1,51 @@
 
 type Props = {}
 
+// import { motion } from "framer-motion";
+
+// const Hero = (props: Props) => {
+//   return (
+//     <div className='flex flex-col lg:gap-7 w-[90%] lg:w-[830px] lg:mt-36 mx-auto text-[#1E0734]'>
+//         <div className="flex flex-col leading-none text-[32px] lg:text-8xl font-bold font-['Poppins'] w-[80%] lg:w-[830px] mx-auto">
+//             <motion.h1
+//               initial={{ x: "-1000" }}
+//             >
+//               Join Our Artistic
+//             </motion.h1>
+//             <h1 className='self-end'>Revolution</h1>
+//         </div>
+//         <p className='text-center text-xs lg:text-lg font-["Roboto"]'> EARTH is on a mission to transform the Art world as we know it and you’re the piece of the puzzle that makes the picture whole. Being part of Earth makes you a hero, so lookout for the signal, you’re about to save the day! </p>
+//     </div>
+//   )
+// }
+
+// export default Hero
+
+import { motion } from "framer-motion";
+
 const Hero = (props: Props) => {
   return (
     <div className='flex flex-col lg:gap-7 w-[90%] lg:w-[830px] lg:mt-36 mx-auto text-[#1E0734]'>
         <div className="flex flex-col leading-none text-[32px] lg:text-8xl font-bold font-['Poppins'] w-[80%] lg:w-[830px] mx-auto">
-            <h1>Join Our Artistic</h1>
-            <h1 className='self-end'>Revolution</h1>
+            <motion.h1
+              initial={{ x: -1000 }}
+              animate={{ x: 0 }}
+              transition={{ type: "spring", damping: 15 }}
+            >
+              Join Our Artistic
+            </motion.h1>
+            <motion.h1
+              initial={{ x: 1000 }}
+              animate={{ x: 0 }}
+              transition={{ type: "spring", damping: 15, duration: 2 }}
+              className='self-end'
+            >
+              Revolution
+            </motion.h1>
         </div>
         <p className='text-center text-xs lg:text-lg font-["Roboto"]'> EARTH is on a mission to transform the Art world as we know it and you’re the piece of the puzzle that makes the picture whole. Being part of Earth makes you a hero, so lookout for the signal, you’re about to save the day! </p>
     </div>
   )
 }
 
-export default Hero
+export default Hero;
--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -1,48 +1,68 @@
-import { Link } from 'react-router-dom'
-import { menu } from '../../assets/home'
-import Button from '../global/Button'
-import { useState } from 'react';
+import { Link } from "react-router-dom";
+import { menu } from "../../assets/home";
+import Button from "../global/Button";
+import { useState } from "react";
+
+import { motion } from "framer-motion";
 
 type Props = {
-    logo: string;
-    fontColor?: string;
-}
+  logo: string;
+  fontColor?: string;
+};
 
 const navList = [
-    { name: 'About', link: '/'},
-    { name: 'Roadmap', link: '/roadmap'},
-    { name: 'FAQs', link: '/'},
-    { name: 'NFT marketplace', link: '/'},
-]
+  { name: "About", link: "/" },
+  { name: "Roadmap", link: "/roadmap" },
+  { name: "FAQs", link: "/" },
+  { name: "NFT marketplace", link: "/" },
+];
 
-const navbar = ({logo, fontColor}: Props) => {
-    const [showNav, setShowNav] = useState(false);
+const navbar = ({ logo, fontColor }: Props) => {
+  const [showNav, setShowNav] = useState(false);
 
   return (
-    <div className='relative'>
-        <div className='flex justify-between items-center w-[90%] mx-auto my-8'>
-            <div>
-                <img src={logo} alt="Earth Logo" />
-            </div>
-            <ul className={`lg:flex gap-7 font-semibold text-[#1E0734] font-["Poppins"] hidden text-[${fontColor}]`}>
-                {navList.map(nav => (
-                    <Link to={nav.link} key={nav.name}>{nav.name}</Link>
-                ))}
-            </ul>
-            <div className='hidden lg:block'>
-                <Button buttonType='button' text='Join Community'/>
-            </div>
-            <img src={menu} alt="menu" className='lg:hidden' onClick={() => setShowNav(!showNav)}/>
+    <motion.div
+      initial={{ y: -1000 }} // Initial position above the viewport
+      animate={{ y: 0 }} // Animate to the top of the viewport
+      transition={{ duration: 1.3, damping: 15}}
+      className="relative"
+    >
+      <div className="flex justify-between items-center w-[90%] mx-auto my-8">
+        <div>
+          <img src={logo} alt="Earth Logo" />
         </div>
-        { showNav && (
-            <ul className={`flex flex-col justify-center items-center w-full absolute gap-5 py-5 z-10 font-semibold bg-white text-[#1E0734]`}>
-                {navList.map(nav => (
-                    <Link to={nav.link} key={nav.name}>{nav.name}</Link>
-                ))}
-            </ul>
-        )}
-    </div>
-  )
-}
+        <ul
+          className={`lg:flex gap-7 font-semibold text-[#1E0734] font-["Poppins"] hidden text-[${fontColor}]`}
+        >
+          {navList.map((nav) => (
+            <Link to={nav.link} key={nav.name}>
+              {nav.name}
+            </Link>
+          ))}
+        </ul>
+        <div className="hidden lg:block">
+          <Button buttonType="button" text="Join Community" />
+        </div>
+        <img
+          src={menu}
+          alt="menu"
+          className="lg:hidden"
+          onClick={() => setShowNav(!showNav)}
+        />
+      </div>
+      {showNav && (
+        <ul
+          className={`flex flex-col justify-center items-center w-full absolute gap-5 py-5 z-10 font-semibold bg-white text-[#1E0734]`}
+        >
+          {navList.map((nav) => (
+            <Link to={nav.link} key={nav.name}>
+              {nav.name}
+            </Link>
+          ))}
+        </ul>
+      )}
+    </motion.div>
+  );
+};
 
-export default navbar
+export default navbar;
